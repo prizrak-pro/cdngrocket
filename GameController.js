a13122cb153a1fe3b4efdbbe0228da470025a7a7
Prizrak <prizrak-sj@yandex.ru>
2024-04-15 19:59:32 +0300

    $

--- GameController.js
+++ GameController.js
@@ -19,6 +19,14 @@ export default class GameController {
 		this.#_ship = _ship;
 	}
 
-    
+    mouseDownMainPage(e)
+    {
+        const currentLayer = this.runtime.layout.getLayer(0);
+        const mouseXYAr = currentLayer.cssPxToLayer(e.clientX, e.clientY, 0);
+        const sprites = this.runtime.objects.MainButtonUI.getAllInstances();
+        if(sprites[0].containsPoint(mouseXYAr[0], mouseXYAr[1])){
+            this.runtime.goToLayout("SelectLevel")
+        }
+    }
 
 }
