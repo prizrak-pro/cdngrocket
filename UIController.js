c615f559402d28d1e5707c39ccd17d0100b415b0
Prizrak <prizrak-sj@yandex.ru>
2024-05-03 20:19:55 +0300

    $

--- UIController.js
+++ UIController.js
@@ -187,7 +187,7 @@ export class UISelectCargo {
         let self = this;
 
 
-        this.runtime.getObjectByUID(8).text = this.gameController.playerConfig.money;
+        this.runtime.GetInstanceByUID(8).text = this.gameController.playerConfig.money;
         this.globalRuntime.objects.Text.getAllInstances().forEach(function(element) {
             element.text = "";
             switch(element.uid)
@@ -227,25 +227,25 @@ export class UISelectCargo {
                     if (self.gameController.ship.priceUpdateMassCargo>0)
                         element.text = self.#numberWithSpaces(self.gameController.ship.priceUpdateMassCargo)
                     else
-                        self.runtime.getObjectByUID(6).destroy();
+                        self.runtime.GetInstanceByUID(6).destroy();
                     break;
                 case 17:
                     if (self.gameController.ship.priceUpdateFuel>0)
                         element.text = self.#numberWithSpaces(self.gameController.ship.priceUpdateFuel)
                     else
-                        self.runtime.getObjectByUID(15).destroy();
+                        self.runtime.GetInstanceByUID(15).destroy();
                     break;
                 case 20:
                     if (self.gameController.ship.priceUpdateShuntingEngine>0)
                         element.text = self.#numberWithSpaces(self.gameController.ship.priceUpdateShuntingEngine)
                     else
-                        self.runtime.getObjectByUID(19).destroy();
+                        self.runtime.GetInstanceByUID(19).destroy();
                     break;
                 case 10:
                     if (self.gameController.ship.priceUpdateStrength>0)
                         element.text = self.#numberWithSpaces(self.gameController.ship.priceUpdateStrength)
                     else
-                        self.runtime.getObjectByUID(9).destroy();
+                        self.runtime.GetInstanceByUID(9).destroy();
                     break;
             }
         });
