8627dfcc9c9409dde7c52dcaebfd2ef405f0beec
Prizrak <prizrak-sj@yandex.ru>
2024-05-09 02:05:42 +0300

    $

--- GameController.js
+++ GameController.js
@@ -297,10 +297,14 @@ export default class GameController {
 			FireMiniRight.setAnimation("1", "beginning");
 			this.fuelConsumptionShuntingEngine();
             this.globalVar.instVars.powerShuntingR1 = (this.globalVar.instVars.powerShuntingR1==0)?2:this.globalVar.instVars.powerShuntingR1;
+
+            this.#_mobButUi[2].animationFrame = 1;
         } else {
 			const FireMiniRight = this.runtime.objects.FireMiniRight.getFirstInstance();
 			FireMiniRight.setAnimation("2", "beginning");
             this.globalVar.instVars.powerShuntingR1 = (this.globalVar.instVars.powerShuntingR1==1)?3:this.globalVar.instVars.powerShuntingR1;
+
+            this.#_mobButUi[2].animationFrame = 0;
         }
     }
 
@@ -312,10 +316,14 @@ export default class GameController {
 			FireMiniLeft.setAnimation("1", "beginning");
 			this.fuelConsumptionShuntingEngine();
             this.globalVar.instVars.powerShuntingL1 = (this.globalVar.instVars.powerShuntingL1==0)?2:this.globalVar.instVars.powerShuntingL1;
+
+            this.#_mobButUi[1].animationFrame = 1;
         } else {
 			const FireMiniLeft = this.runtime.objects.FireMiniLeft.getFirstInstance();
 			FireMiniLeft.setAnimation("2", "beginning");
             this.globalVar.instVars.powerShuntingL1 = (this.globalVar.instVars.powerShuntingL1==1)?3:this.globalVar.instVars.powerShuntingL1;
+
+            this.#_mobButUi[1].animationFrame = 0;
         }
     }
 
