f55f84b431f8349ea47600754b5c6edde94526d5
Prizrak <prizrak-sj@yandex.ru>
2024-05-30 22:44:49 +0300

    $

--- Functions.js
+++ Functions.js
@@ -2,14 +2,14 @@
 
 export default class Functions {
 
-    static classMap = {
-        PlayerConfig: PlayerConfig,
-    }
+    // static classMap = {
+    //     PlayerConfig: PlayerConfig,
+    // }
 
-    static deserializeObjectPlayer = (string) => {
-        let obj = JSON.parse(string)
-        return new classMap[obj.className](JSON.parse(obj));
-    }
+    // static deserializeObjectPlayer = (string) => {
+    //     let obj = JSON.parse(string)
+    //     return new classMap[obj.className](JSON.parse(obj));
+    // }
 
     static isEmptyObject(obj) {
         for (var i in obj) {
