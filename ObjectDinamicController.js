2f6209ddb3fa0ed24aec33892c77e7f42057eb99
Prizrak <prizrak-sj@yandex.ru>
2024-08-28 21:55:40 +0300

    $

---
+++ ObjectDinamicController.js
@@ -0,0 +1,34 @@
+export default class ObjectDinamicController {
+
+    runtime;
+    #_dict;
+    playerConfig;
+    levelConfig;
+    yandexSDC;
+    globalVar;
+    
+    Functions
+    GameConfig;
+    UISelectCargo;
+    #LevelConfig;
+    #DictConroller;
+    #PlayerConfig;
+    #Ship;
+
+    //временные
+    #_shipVisual;
+    #_uiMainGame;
+    #_powerState = [false, false, false];
+    #_mobButUi = [null, null, null];
+    #_engine_shutdown = true;
+
+    constructor(runtime) {
+
+    }
+
+    init(level)
+    {
+
+    }
+
+}
