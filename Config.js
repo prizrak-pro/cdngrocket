a1cea61b9f104d340e4d92b49b219bd580742756
Prizrak <prizrak-sj@yandex.ru>
2024-10-21 00:27:45 +0300

    $

--- Config.js
+++ Config.js
@@ -1,11 +1,11 @@
 export class PlayerConfig {
-	#maxOpenLevel = 4;
+	#maxOpenLevel = 0;
 
 	#money = 0;
 
 	#ship = {
 		type: 0,
-		levelMass: 2,
+		levelMass: 0,
 		levelFuel: 0,
 		levelShuntingEngine: 0,
 		levelStrength: 0
