e2d97bc383ad45d05b94da5d0c3cb057031129f1
Prizrak <prizrak-sj@yandex.ru>
2024-10-27 03:33:08 +0300

    $

--- ShipController.js
+++ ShipController.js
@@ -6,7 +6,7 @@ export default class Ship {
 
 	#_levelShip = 0;
 	//#_coefMainEngine = [1.8, 1.7, 1.6, 1.5, 1.4, 1.2];
-	#_coefMainEngine = [1.8, 1.7, 1.6, 1.5, 1.4, 0.9];
+	#_coefMainEngine = [1.8, 1.6, 1.4, 1.2, 1.1, 0.9];
 	#_maxMass=[200, 400, 600, 800, 1000, 1200];
 	#_priceLevelShip=[400, 4000, 12000, 28000, 44000];
 	#_massCargo=0;
